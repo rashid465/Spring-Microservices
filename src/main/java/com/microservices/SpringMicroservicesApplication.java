51392580bb0197c42b203a735c53d9aa1ea6a89b
rashid465 <sufyanashif@gmail.com>
2024-02-13 12:26:18 +0530

    Hello Added

--- src/main/java/com/microservices/SpringMicroservicesApplication.java
+++ src/main/java/com/microservices/SpringMicroservicesApplication.java
@@ -8,6 +8,7 @@ public class SpringMicroservicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMicroservicesApplication.class, args);
+		System.out.println("Hello ");
 	}
 
 }
